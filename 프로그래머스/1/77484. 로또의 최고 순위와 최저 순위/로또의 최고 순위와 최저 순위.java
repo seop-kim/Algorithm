ba22944d27eaca045e80a6af4b8905e272c5b233
seop <77163842+seop-kim@users.noreply.github.com>
2023-11-29 17:05:45 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+		int[] answer = new int[2];
+		int count = 0, zeroCheck = 0;
+
+		for (int i = 0; i < lottos.length; i++) {
+
+			if (lottos[i] == 0) {
+				zeroCheck++;
+				continue;
+			}
+
+			for (int j = 0; j < win_nums.length; j++) {
+				if (lottos[i] == win_nums[j]) {
+					count++;
+				}
+			}
+		}
+
+		for (int i = 1; i >= 0; i--) {
+			for (int j = 0; j < 6; j++) {
+				if (count == 6) {
+					answer[i] = 1;
+				} else if (count == 5) {
+					answer[i] = 2;
+				} else if (count == 4) {
+					answer[i] = 3;
+				} else if (count == 3) {
+					answer[i] = 4;
+				} else if (count == 2) {
+					answer[i] = 5;
+				} else {
+					answer[i] = 6;
+				}
+			}
+			count += zeroCheck;
+		}
+
+		return answer;
+    }
+}
