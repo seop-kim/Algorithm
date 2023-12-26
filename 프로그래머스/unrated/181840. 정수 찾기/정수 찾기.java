4f370994b94e7ce374831475a06f840c9e24e9ee
seop <77163842+seop-kim@users.noreply.github.com>
2023-12-26 18:42:22 +0900

    [level unrated] Title: 정수 찾기, Time: 0.01 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+      int answer = 0;
+
+        for (int num : num_list) {
+            if (n == num) {
+                return 1;
+            }
+        }
+        return answer;
+    }
+}
