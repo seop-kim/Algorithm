1c59d104a6bd92333cd600b4a776f335d759ce96
seop <77163842+seop-kim@users.noreply.github.com>
2024-04-16 02:14:59 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] charArr = my_string.toCharArray();
+        char temp = charArr[num1];
+        
+        charArr[num1] = charArr[num2];
+        charArr[num2] = temp;
+        
+        
+        return new String(charArr);
+    }
+}
