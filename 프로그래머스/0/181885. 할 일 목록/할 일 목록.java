fd8d5b166ef56ab6d36594b1f29abd3c586192f6
seop <77163842+seop-kim@users.noreply.github.com>
2024-05-01 17:59:15 +0900

    [level 0] Title: 할 일 목록, Time: 10.10 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String answer = "";
+        for(int i = 0; i < todo_list.length; i++){
+            if(!finished[i]){
+                answer += todo_list[i] + " ";
+            }
+        }
+        return answer.split(" ");
+    }
+}
